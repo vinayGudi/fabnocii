a5ba99600a3055b9b1de4ebaeba23b484601480f
vinayGudi <37874414+vinayGudi@users.noreply.github.com>
2018-04-04 19:52:21 +0530

    Update main_code.cpp

--- main_code.cpp
+++ main_code.cpp
@@ -2,7 +2,36 @@
 #include<iostream>
 using namepsace std;
 int main(){
-	//to do fab
-
+	#include<iostream>
+#include<pthread.h>
+#include <unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+using namespace std;
+	 int size,first=0,second=1,third,n;
+	 int a[1000];
+void * cal(void * arg)
+{
+  for(int i=0;i<n;i++)
+  {
+	  	first=a[i];
+	  	third=first+second;
+	  	first=second;
+	  	second=third;
+  }
+}
+int main(){  cout<<"enter number of elements"<<endl;
+	  cin>>n;
+      pthread_t fd;
+      pthread_create(&fd,NULL,cal,NULL);
+      pthread_join(fd,NULL);
+      wait(1);
+        for(int i=0;i<n;i++)
+  {   //cout<<"in parent thread"<<endl;
+	  	cout<<a[i]<<" ";
+	  	
+  }
+	
+}
 
 }
